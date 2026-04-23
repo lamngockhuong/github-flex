5a20000b858b208f09deedb228bda0da958f65f3
Lâm Ngọc Khương <me@ngockhuong.com>
2026-04-23 13:48:13 +0700

    feat(website): add ohlab.dev credit to footer (#62)
    Add "Built by ohlab.dev" with link below the copyright line,
    with i18n support for EN, VI, and JA locales.

--- website/src/constants.ts
+++ website/src/constants.ts
@@ -3,6 +3,7 @@
 export const CHROME_STORE_URL = 'https://chromewebstore.google.com/detail/github-flex/iechckkdnjmdnpbdohhnmojofcbfnemc';
 export const FIREFOX_ADDONS_URL = 'https://addons.mozilla.org/en-US/firefox/addon/github-flex/';
 export const GITHUB_URL = 'https://github.com/lamngockhuong/github-flex';
+export const OHLAB_URL = 'https://ohlab.dev';
 
 export const OTHER_PROJECTS = [
   {
